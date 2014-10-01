ba9caa545c824a1535cf0ca7096e27d59d20c643
Mattias Wikström <mw222rs@student.lnu.se>
2014-10-01 22:23:27 +0200

    start

---
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+    class SecretNumber
+    {
+        private int _count;
+        private int _number;
+
+        public const int MaxNumberOfGuesses = 7;
+
+        public void Initialize 
+        {
+        }
+
+        public bool MakeGuess(int number)
+        {
+            return true;
+        }
+
+
+    }
+}
